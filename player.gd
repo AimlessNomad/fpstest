5067584c5f3f6d5ab432c591566bb7b7a3dd3823
AimlessNomad <mascislarson@gmail.com>
2024-07-30 13:15:17 -0500

    added basic enemy pathfinding

--- player.gd
+++ player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 
+signal newPos
 
 const JUMP_VELOCITY = 4.5
 const SPEED = 1.2
@@ -32,7 +33,7 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -54,3 +55,4 @@ func _physics_process(delta):
 	
 	
 	move_and_slide()
+	newPos.emit(position.x, position.z)
